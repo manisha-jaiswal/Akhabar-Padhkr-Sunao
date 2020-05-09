ed1052c854b8a847c3cb33e84b9bfd4797e9836a
MANISHA KUMARI <Manisha16041998@gmail.com>
2020-05-09 16:33:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def speak(str):
 
 if __name__ == '__main__':
     speak("News for today.. Lets begin")
-   # url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=6d34b621f6d74729a068cd7a9ff70e47"
-    url="http://newsapi.org/v2/top-headlines?country=in&apiKey=6d34b621f6d74729a068cd7a9ff70e47"
+  
+    url="your upi"
     news = requests.get(url).text
     news_dict = json.loads(news)
     arts = news_dict['articles']
